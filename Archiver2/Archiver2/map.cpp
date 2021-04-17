16a8c6eaa460d98021188e98d422a5cd09455bf5
Oleksandr <OlexandrTanasienko@gmail.com>
2021-04-17 20:56:00 +0300

    Initialize dictionary with single symbols

---
+++ Archiver2/Archiver2/map.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	string pathToFile = "in.txt";
+	//cin>>pathToFile;
+	
+	char a[] = "aaaafbc gcncn ";
+	ofstream outFile(pathToFile, ios::binary);
+	outFile.write(a, sizeof(char) * 14);
+	outFile.close();
+	
+	unordered_map<string, int> dictionary;
+	for(int i=0;i<=255;i++)
+	{
+		dictionary.insert(make_pair(to_string(char(i)), i));
+	}
+
+	
+	ifstream inFile(pathToFile, ios::binary);
+	char byte;
+	vector<int> binaryEncoding;
+	while (inFile.read(&byte, sizeof(char)))
+	{
+		cout<<byte<<" ";
+		
+	}
+}
+
